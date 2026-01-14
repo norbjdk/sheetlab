7ebc9b471e0e8d3f16f79404323cca693e901d28
ndominkiewicz <norbertd1410@gmail.com>
2026-01-14 01:54:53 +0100

    build: add ikonli javafx requirement for project module

--- desktop/src/main/java/module-info.java
+++ desktop/src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.norbdev.desktop {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
 
     opens com.norbdev.desktop to javafx.fxml;
     exports com.norbdev.desktop;
