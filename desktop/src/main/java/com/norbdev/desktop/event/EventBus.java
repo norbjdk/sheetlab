de635530e0192f3c20cec6e0a808b7fb0cf8db6c
ndominkiewicz <norbertd1410@gmail.com>
2026-01-19 20:11:33 +0100

    feat(desktop) add simple eventBus mechanism
    - Add publish
    - Add subscribe

--- desktop/src/main/java/com/norbdev/desktop/event/EventBus.java
+++ desktop/src/main/java/com/norbdev/desktop/event/EventBus.java
@@ -1,4 +1,33 @@
 package com.norbdev.desktop.event;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
 public class EventBus {
+    private static EventBus instance;
+    private Map<Class<?>, List<Consumer>> listeners = new HashMap<>();
+
+    private EventBus() {}
+
+    public static EventBus getInstance() {
+        if (instance == null) {
+            instance = new EventBus();
+        }
+        return instance;
+    }
+
+    public <T> void subscribe(Class<T> eventType, Consumer<T> listener) {
+        listeners.computeIfAbsent(eventType, k -> new ArrayList<>()).add(listener);
+    }
+
+    public <T> void publish(T event) {
+        Class<?> eventClass = event.getClass();
+        List<Consumer> eventListeners = listeners.get(eventClass);
+        if (eventListeners != null) {
+            eventListeners.forEach(listener -> listener.accept(event));
+        }
+    }
 }
