543672b6a00e6655c153a9d99940843d873355dd
ndominkiewicz <norbertd1410@gmail.com>
2026-01-19 20:44:54 +0100

    feat(server) add server module based on SpringBoot
    - Default project
    - Nothing add yet

---
+++ server/src/test/java/com/norbjdk/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norbjdk.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
