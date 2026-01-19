06d1c3b163173724d14bae7a8b8fec2e1e6013dd
ndominkiewicz <norbertd1410@gmail.com>
2026-01-19 21:18:45 +0100

    refactor(desktop) renamed from HomeController

--- desktop/src/main/java/com/norbdev/desktop/controller/MainController.java
+++ desktop/src/main/java/com/norbdev/desktop/controller/MainController.java
@@ -2,18 +2,16 @@ package com.norbdev.desktop.controller;
 
 import com.norbdev.desktop.component.NavigationBar;
 import com.norbdev.desktop.component.SideBar;
-import com.norbdev.desktop.view.IntroView;
+import com.norbdev.desktop.view.HomeView;
 import com.norbdev.desktop.view.NewProjectView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HomeController implements Initializable {
+public class MainController implements Initializable {
     private final BorderPane layoutContainer = new BorderPane();
     private final SideBar sideBar = new SideBar();
     private final NavigationBar navigationBar = new NavigationBar();
@@ -22,7 +20,7 @@ public class HomeController implements Initializable {
      * Views
      */
 
-    private final IntroView introView = new IntroView();
+    private final HomeView homeView = new HomeView();
     private final NewProjectView newProjectView = new NewProjectView();
 
     @FXML private BorderPane root;
